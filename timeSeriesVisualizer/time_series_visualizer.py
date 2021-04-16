d5eb38dc26b133e228ce7a19e8879453ad382a70
LenyPython <buniadyserv@gmail.com>
2021-04-16 18:58:13 +0200

    bar chart completion

--- timeSeriesVisualizer/time_series_visualizer.py
+++ timeSeriesVisualizer/time_series_visualizer.py
@@ -15,7 +15,12 @@ df = df[(df['value'] >= df['value'].quantile(0.025)) &
 def draw_line_plot():
 	# Draw line plot
 	fig, axs = plt.subplots(figsize=(20,9))
-	axs = df['value'].plot(xlabel='Date', ylabel='Page Views',style=['r'], title='Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+	df['value'].plot(xlabel='Date',
+					ax=axs,
+					ylabel='Page Views',
+					style=['r'],
+					title='Daily freeCodeCamp Forum Page Views 5/2016-12/2019'
+						)
 	# Save image and return fig (don't change this part)
 	fig.savefig('line_plot.png')
 	return fig
@@ -23,16 +28,18 @@ draw_line_plot()
 
 def draw_bar_plot():
 	# Copy and modify data for monthly bar plot
-	df_bar = df.copy()
-	df_bar['month'] = df_bar.index.month
+	df_bar = pd.DataFrame(df.resample('M').mean())
+	df_bar['Months'] = df_bar.index.month_name()
 	df_bar.index = df_bar.index.year
+	df_bar = df_bar.pivot(columns='Months', values='value')
+	df_bar = df_bar[['January', 'February','March','April','May','June','July','August','September','October','November','December']]
 
-	print(df_bar)
 	# Draw bar plot
-	fig, axs = plt.subplots(figsize=(12,16))
-	axs = df_bar.plot(kind='bar',
-			xlablel='Years',
-			ylablel='Average Page Views',
+	fig, axs = plt.subplots(figsize=(12,9))
+	df_bar.plot(kind='bar',
+			ax=axs,
+			xlabel='Years',
+			ylabel='Average Page Views',
 			)
 
 	# Save image and return fig (don't change this part)
@@ -41,18 +48,20 @@ def draw_bar_plot():
 draw_bar_plot()
 
 def draw_box_plot():
-    # Prepare data for box plots (this part is done!)
-    df_box = df.copy()
-    df_box.reset_index(inplace=True)
-    df_box['year'] = [d.year for d in df_box.date]
-    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+	# Prepare data for box plots (this part is done!)
+	df_box = df.copy()
+	df_box.reset_index(inplace=True)
+	df_box['year'] = [d.year for d in df_box.date]
+	df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
-    # Draw box plots (using Seaborn)
-
-
+	# Draw box plots (using Seaborn)
 
 
 
-    # Save image and return fig (don't change this part)
-    fig.savefig('box_plot.png')
-    return fig
+
+
+
+	# Save image and return fig (don't change this part)
+	fig.savefig('box_plot.png')
+	return fig
+draw_box_plot()
